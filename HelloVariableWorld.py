e25be727ed9003b0ef2ff26ce6cc1472baa7f66a
Abe17 <vb_abraham22@hotmail.com>
2018-09-16 21:15:32 -0700

    first version

---
+++ HelloVariableWorld.py
@@ -0,0 +1,33 @@
+# create a variable called name that holds a string
+name = "Abraham Valdez Bravo"
+
+# Create a variable called country that holds a string
+country = "Mexico"
+
+# Create a variable called age that holds the integer
+age = 38
+
+# create a variable called hourly_wage
+hrs_wage = 50
+
+# Calculate daily wage for abe
+Daily_wage = hrs_wage * 8
+
+# Create a variable calld Satisfied that holds a boolean
+satisfied = True
+
+# Print out Hello <name>!
+print("Hello" + name + "!")
+
+# Print th country of the user
+print("You live in" + country)
+
+# print our user age
+print("You are" + age + "years old")
+
+# Print out daily wages
+print("You make" + str(Daily_wage)+ "per day")
+
+#is the user satisfied
+print("Are you satisfied wiht your current age?" + str(satisfied))
+
